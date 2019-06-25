9c75697dc15ef68315814148aa5f2e2780eaa2cd
Yutaka Izumaru <y.izumaru@arrvis.com>
2019-06-25 21:33:12 +0900

    remove IBInspectable

--- ArrvisCore/Extensions/UIViewEx.swift
+++ ArrvisCore/Extensions/UIViewEx.swift
@@ -31,41 +31,6 @@ extension UIView {
     }
 }
 
-// MARK: - IBInspectable
-extension UIView {
-
-    /// 角Radius
-    @IBInspectable public var cornerRadius: CGFloat {
-        get {
-            return layer.cornerRadius
-        }
-        set {
-            layer.cornerRadius = newValue
-            layer.masksToBounds = newValue > 0
-        }
-    }
-
-    /// 枠線太さ
-    @IBInspectable public var borderWidth: CGFloat {
-        get {
-            return self.layer.borderWidth
-        }
-        set {
-            self.layer.borderWidth = newValue
-        }
-    }
-
-    /// 枠線色
-    @IBInspectable public var borderColor: UIColor? {
-        get {
-            return UIColor(cgColor: self.layer.borderColor!)
-        }
-        set {
-            self.layer.borderColor = newValue?.cgColor
-        }
-    }
-}
-
 // MARK: - SubView Property
 extension UIView {
 
