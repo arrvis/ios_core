aa112084e3a73e677a4b7a1ad93e15e639b7974a
Yutaka Izumaru <y.izumaru@arrvis.com>
2019-06-26 00:19:57 +0900

    revert to UITabBarEx

---
+++ ArrvisCore/Extensions/UITabBarEx.swift
@@ -0,0 +1,36 @@
+//
+//  HeightChangeableTabBar.swift
+//  ArrvisCore
+//
+//  Created by Yutaka Izumaru on 2019/06/25.
+//  Copyright © 2019 Arrvis Co., Ltd. All rights reserved.
+//
+
+import UIKit
+
+private var overrideHeightKey = 0
+
+extension UITabBar {
+
+    /// 高さオーバーライド
+    public var overrideHeight: CGFloat? {
+        get {
+            return objc_getAssociatedObject(self, &overrideHeightKey) as? CGFloat
+        }
+        set {
+            objc_setAssociatedObject(self, &overrideHeightKey, newValue, .OBJC_ASSOCIATION_RETAIN)
+        }
+    }
+
+    open override func sizeThatFits(_ size: CGSize) -> CGSize {
+        super.sizeThatFits(size)
+        guard let window = UIApplication.shared.keyWindow else {
+            return super.sizeThatFits(size)
+        }
+        var sizeThatFits = super.sizeThatFits(size)
+        if let overrideHeight = overrideHeight {
+            sizeThatFits.height = overrideHeight + window.safeAreaInsets.bottom
+        }
+        return sizeThatFits
+    }
+}
