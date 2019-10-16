6f034f738909d8fd1958a07ec0641aba4077aa50
Yutaka Izumaru <y.izumaru@arrvis.com>
2019-10-16 23:10:51 +0900

    仕方がない...

--- ArrvisCore/VIPER/ViewInterface.swift
+++ ArrvisCore/VIPER/ViewInterface.swift
@@ -6,5 +6,5 @@
 //  Copyright © 2018年 Arrvis Co., Ltd. All rights reserved.
 //
 
-public protocol ViewInterface: class, LoadingShowable {
+public protocol ViewInterface: LoadingShowable where Self: UIViewController {
 }
