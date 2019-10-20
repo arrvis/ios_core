d8b1cb37a5952616b58ff2ed3c80a9aefd54a40b
Yutaka Izumaru <y.izumaru@arrvis.com>
2019-10-21 01:46:53 +0900

    add description

--- ArrvisCore/Services/Google/EventModels.swift
+++ ArrvisCore/Services/Google/EventModels.swift
@@ -24,6 +24,7 @@ public struct GoogleEvent: BaseModel {
     public let originalStartTime: GoogleDatetime?
     public let recurrence: [String]?
     public let sequence: Int?
+    public let description: String?
     public let status: String
 
     public var updatedAt: Date? {
