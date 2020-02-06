7e20aca56bbf5cc3ea863dc83fd01e65cd0782be
Yutaka Izumaru <y.izumaru@arrvis.com>
2020-02-07 06:21:15 +0900

    add URLEx

---
+++ ArrvisCore/Core/Extensions/URLEx.swift
@@ -0,0 +1,25 @@
+//
+//  URLEx.swift
+//  ArrvisCore
+//
+//  Created by Yutaka Izumaru on 2020/02/07.
+//  Copyright © 2020 Arrvis Co., Ltd. All rights reserved.
+//
+
+import Photos
+
+extension URL {
+
+    func generateThumbnail() -> UIImage? {
+        let imageGenerator = AVAssetImageGenerator(asset: AVAsset(url: self))
+        imageGenerator.appliesPreferredTrackTransform = true
+        do {
+            let imageRef
+                = try imageGenerator.copyCGImage(at: CMTimeMake(value: Int64(0), timescale: 1), actualTime: nil)
+            let image = UIImage(cgImage: imageRef)
+            return image
+        } catch {
+            return nil
+        }
+    }
+}
