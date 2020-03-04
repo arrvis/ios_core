498ccc27ba8fcc0f4069aa40514de3f0ea9aa66c
Yutaka Izumaru <y.izumaru@arrvis.com>
2020-03-05 02:09:55 +0900

    これかな

--- ArrvisCore/Navigate/BaseRootViewController.swift
+++ ArrvisCore/Navigate/BaseRootViewController.swift
@@ -197,6 +197,12 @@ open class BaseRootViewController: UIViewController {
             .subscribe(onNext: { _ in
                 SwiftEventBus.post(SystemBusEvents.currentViewControllerChanged)
             }).disposed(by: self)
+        if let vc = vc as? UINavigationController {
+            vc.topViewController?.rx.methodInvoked(#selector(UIViewController.dismiss))
+                .subscribe(onNext: { _ in
+                    SwiftEventBus.post(SystemBusEvents.currentViewControllerChanged)
+                }).disposed(by: self)
+        }
     }
 }
 
